8959bed076011f3a96dab3f6658ade102c885839
emilysr <emily.rasmussen@live.dk>
2016-09-26 15:42:27 +0200

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -2,13 +2,13 @@ package terning;
 
 public class Dice {
 	private final int MAX = 6;
-	// roll the die and return the value (1-6)
+	// roll the die and return the value (1-6).
 	public int roll() {
 		int d1=(int)(Math.random()*MAX)+1;     // Finder et tilfældigt tal fra 1-6
 		return d1;                     // 1-6		
 	}
 	
-	// roll the die n times and print the values
+	// roll the die n times and print the values.
 	public void rollMultiple(int n) {
 		for (int i=0; i<n; i++) {
 			System.out.print(roll() + " ");
