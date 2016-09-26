e2bd2b44e282fafd3a0cf3e7d727be629657bb65
SimonChr <simonchristiansen1@gmail.com>
2016-09-26 15:42:14 +0200

    Update Dice.java
    Rettet kommentar på tegning delen

--- Dice.java
+++ Dice.java
@@ -4,7 +4,7 @@ public class Dice {
 	private final int MAX = 6;
 	// roll the die and return the value (1-6)
 	public int roll() {
-		int d1=(int)(Math.random()*MAX)+1;     // 0-1
+		int d1=(int)(Math.random()*MAX)+1;     // Finder et tilfældigt tal fra 1-6
 		return d1;                     // 1-6		
 	}
 	
